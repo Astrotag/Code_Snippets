44bd7efe9fc51d854152efbda4509254c1611ba4
Greig Myles <36883641+Astrotag@users.noreply.github.com>
2019-10-21 14:07:11 +0100

    Add files via upload

---
+++ Code_Snippets/src/greigmmyles/coins/CoinInventoryEmptyException.java
@@ -0,0 +1,18 @@
+package greigmmyles.coins;
+
+public class CoinInventoryEmptyException extends Exception
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8491608550152818792L;
+
+    public CoinInventoryEmptyException(String message)
+    {
+        super(message);
+    }
+    
+    
+
+}
